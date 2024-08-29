97c7d11d23f02137f735764e253ca40bbc6a8449
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-08-29 10:34:17 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
